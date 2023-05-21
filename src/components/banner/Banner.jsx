6bfc7eb2a09d8f5d218d9aa3aa358bff048f77d6
mphoChuene <mphochuene42@gmail.com>
2023-05-21 22:35:33 +0200

    formatting & spacing

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import styles from "./Banner.module.css";
+import blue_arrow from "../img/blue_Arrow.svg";
+import blue_logo from "../img/blue_logo.svg";
+import yellow_arrow from "../img/yellow_arrow.svg";
+import yellow_logo from "../img/yellow_logo.svg";
+import main_logo from "../img/main_logo.svg";
 
 const Banner = () => {
   return (
@@ -9,32 +14,47 @@ const Banner = () => {
         <div className={styles.main_title}>
           <h1>EXPLORING WEB3 TECHNOLOGY</h1>
         </div>
-        <p>
-          it is an umbrella term that encompasses various technologies and
-          protocols that aim to create a more open, transparent, and
-          decentralized web
-        </p>
+        <div className={styles.sub_logo}>
+          <p>
+            it is an umbrella term that encompasses various technologies and
+            protocols that aim to create a more open, transparent, and
+            decentralized web
+          </p>
+          <img src={main_logo} alt="" />
+        </div>
       </div>
 
       {/*   subsection part */}
       <div className={styles.sub_container}>
         <div className={styles.sub_section1}>
-          <h2>OUR FUTURE WITH WEB3</h2>
+          <div className={styles.sub_logo}>
+            <h2>OUR FUTURE WITH WEB3</h2>
+            <img src={blue_arrow} alt="/" />
+          </div>
           <div className={styles.main_title}>
-            <p>
-              Technologies have the potential to revolutionize many industries
-              and aspects of our live
-            </p>
+            <div className={styles.sub_logo}>
+              <p>
+                Technologies have the potential to revolutionize many industries
+                and aspects of our live
+              </p>
+              <img src={blue_logo} size={2} alt="/" />
+            </div>
           </div>
         </div>
 
         <div className={styles.sub_section2}>
-          <h2>WEB3, ETHEREUM AND OTHER BLOCKCHAIN NETWORKS</h2>
+          <div className={styles.sub_logo}>
+            <h2>WEB3, ETHEREUM AND OTHER BLOCKCHAIN NETWORKS</h2>
+            <img src={yellow_arrow} alt="/" />
+          </div>
           <div className={styles.main_title}>
-            <p>
-              Web3 is a set of tools and protocols that allow decentralized
-              applications to interact with the blockchain
-            </p>
+            <div className={styles.sub_logo}>
+              <p>
+                Web3 is a set of tools and protocols that allow decentralized
+                applications to interact with the blockchain
+              </p>
+              <img src={yellow_logo} alt="/" />
+            </div>
           </div>
         </div>
       </div>
